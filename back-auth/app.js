f5d65268fc4e7ea77a2656ecd4cdca705e1125bc
vonbarown <vonielbrown@pursuit.org>
2020-02-02 15:55:04 -0500

    heroku test

--- back-auth/app.js
+++ back-auth/app.js
@@ -25,7 +25,7 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 
