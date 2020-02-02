b8675881d679e821b41c2e2bb9e7b1ab6f4d8162
vonbarown <vonielbrown@pursuit.org>
2020-02-02 10:48:20 -0500

    creating scaffold for the app to be built on

--- front-end-auth/src/App.js
+++ front-end-auth/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Navbar } from './Components/Navbar'
+import { Switch, Route } from 'react-router-dom'
+import AuthContainer from './Containers/AuthContainer'
+import { Home } from './Components/Home'
+import { Users } from './Components/Users'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route path='/login' component={AuthContainer} />
+        <Route path='/signup' component={AuthContainer} />
+        <Route path='/users' component={Users} />
+        <Route path='/' component={Home} />
+      </Switch>
     </div>
   );
 }
---
+++ front-end-auth/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <Link to="/">Home</Link>{" "}
+            <Link to='login'>Log-In</Link>{' '}
+            <Link to='/signup'>Sign-Up</Link>{' '}
+            <Link to='/users'>Log-out</Link>
+        </nav>
+    )
+}
